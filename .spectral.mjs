0db7e605b031bfe961c5d55a91eacaa8b64fbc8f
laconic_cactus.0p@icloud.com <laconic_cactus.0p@icloud.com>
2024-03-12 14:35:52 +0500

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE2MDk2Mw/spectral.js?branch_slug=main&token=61c86093-13c1-458d-bca1-6b9e74f67a13";
+export default { extends: ruleset };
